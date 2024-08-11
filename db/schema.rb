f1d45af8e448444e520eeaedb2c697c0cbce07a8
shishi <shishi@shishinoMacBook-Pro.local>
2024-08-12 05:53:18 +0900

    メール送信機能の作成2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_27_193119) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_10_102126) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -150,6 +150,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_27_193119) do
     t.datetime "updated_at", null: false
     t.integer "age"
     t.string "icon_image"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

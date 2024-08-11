e3fba1bf335f47518343d087b7d48f30b761d253
shishi <shishi@shishinoMacBook-Pro.local>
2024-08-12 06:43:21 +0900

    production/defaulturl変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_production"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {  :host => 'gamefriend333.com', protocol: 'https'  }
+  config.action_mailer.default_url_options = { host: 'gamefriend333.com', protocol: 'https'  }
   #送信方法を指定（この他に:sendmail/:file/:testなどがあります)
   config.action_mailer.delivery_method = :smtp
   #送信方法として:smtpを指定した場合は、このconfigを使って送信詳細の設定を行います

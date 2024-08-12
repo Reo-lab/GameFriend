771c180d9870bb03068fdb24aed6aaac857f5b37
shishi <shishi@shishinoMacBook-Pro.local>
2024-08-12 09:52:47 +0900

    nginx/dockerfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
   config.assets.compile = false
   config.assets.digest = true
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.

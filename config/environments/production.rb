9ef1c8e3ca89645a3279671afd92ee825127b8fd
shishi <shishi@shishinoMacBook-Pro.local>
2024-08-13 18:30:41 +0900

    config.assets.compile = false追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   config.assets.js_compressor = :terser
   config.assets.css_compressor = :sass

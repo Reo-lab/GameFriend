0fab8b5b68469b397e32c305c03fd3df621cf3fb
shishi <shishi@shishinoMacBook-Pro.local>
2024-08-14 14:09:59 +0900

    importchannels修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "channels/chat_channel"
+import "channels"
 import 'bootstrap'

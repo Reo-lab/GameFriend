daeb92e64ed3115003c4ff0b900a351689a0a691
rq-Reo-lab-at-321899771753 <yuniyuniyuniyuni333@gmail.com>
2024-11-13 18:21:41 +0900

    静的OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
       separator: '|',
       description: 'ゲームフレンドは、オンラインゲームで一緒に遊ぶ友達を簡単に見つけられるマッチングサイトです。多くのプレイヤーとつながり、楽しいゲーム時間を共有しましょう。',
       keywords: 'ゲームフレンド, ゲーム仲間探し, オンラインゲーム友達, ゲームマッチング, ゲームコミュニティ, 一緒に遊ぶ友達, マルチプレイヤー友達募集,オーバーウォッチ,ヴァロラント, APEX, ロケットリーグ,',
-      canonical: request.original_url,
+      canonical: 'https://gamefriend333.com/',
       noindex: ! Rails.env.production?,
       icon: [
         { href: image_url('favicon.ico') },
@@ -21,13 +21,14 @@ module ApplicationHelper
         title: 'ゲームフレンド',
         description: 'ゲームフレンドは、オンラインゲームで一緒に遊ぶ友達を簡単に見つけられるマッチングサイトです。多くのプレイヤーとつながり、楽しいゲーム時間を共有しましょう。', 
         type: 'website',
-        url: request.original_url,
-        image: image_url('favicon.ico'),
+        url: 'https://gamefriend333.com/',
+        image: image_url('ogp.png'),
         locale: 'ja_JP',
       },
       twitter: {
         card: 'summary_large_image',
         site: '@ツイッターのアカウント名',
+        image: image_url('ogp.png')
       }
     }
   end
